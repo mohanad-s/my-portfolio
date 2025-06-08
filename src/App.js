692aa4653d207b2c8c6a436df5885b3ca4b752f0
mohanad-s <maaldakheel@stu.kau.edu.sa>
2025-06-08 17:10:55 +0300

    Changes to be committed: 	new file:   public/Mohanad_Aldakheel.pdf 	deleted:    public/Mohanad_Aldakheel_.pdf 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -247,7 +247,7 @@ const copyEmailToClipboard = async () => {
         <h2 className="text-2xl font-bold text-white">My Resume</h2>
         <div className="flex items-center space-x-3">
           <a
-            href="/Mohanad_Aldakheel_.pdf"
+            href="/Mohanad_Aldakheel.pdf"
             download
             className="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg text-sm font-medium transition-colors flex items-center"
           >
